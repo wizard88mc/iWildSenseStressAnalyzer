883bfef8e09f6341747965e91446a4b092840d06
Matteo Ciman <wizard88_mc@yahoo.it>
2015-07-20 15:37:58 +0200

    First commit with data

---
+++ src/iwildsensestressanalyzer/IWildSenseStressAnalyzer.java
@@ -0,0 +1,16 @@
+package iwildsensestressanalyzer;
+
+/**
+ *
+ * @author Matteo
+ */
+public class IWildSenseStressAnalyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/iwildsensestressanalyzer/filereader/IMEIListReader.java
@@ -0,0 +1,28 @@
+/*
+ * 
+ */
+package iwildsensestressanalyzer.filereader;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Matteo
+ */
+public class IMEIListReader {
+
+    private static final String FILE = "imeis.csv";
+    
+    /**
+     * Retrieves the list of IMEI of the participants of the study
+     * @return 
+     */
+    public static ArrayList<String> getIMEIsList() {
+        
+        ArrayList<String> imeis = new ArrayList<>();
+        
+        
+        
+        return imeis;
+    }
+}
